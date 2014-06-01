1d352e4f76bee30a6cb5a1cc3debc77854f437ec
Teemu Frisk <teemu.frisk@gmail.com>
2014-06-01 11:52:47 +0300

    First commit

---
+++ test/sidonnaisuudet/core_test.clj
@@ -0,0 +1,7 @@
+(ns sidonnaisuudet.core-test
+  (:require [clojure.test :refer :all]
+            [sidonnaisuudet.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
