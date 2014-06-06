920cd7f534df1bace1ed3bbe8cd296dfdfa6cad1
Teemu Frisk <teemu.frisk@gmail.com>
2014-06-06 11:27:52 +0300

    Add basic functionality to read data from database

--- src/sidonnaisuudet/core.clj
+++ src/sidonnaisuudet/core.clj
@@ -1,11 +1,1 @@
-(ns sidonnaisuudet.core
-  (:require
-    [clojurewerkz.neocons.rest :as nr]
-    [clojurewerkz.neocons.rest.nodes :as nn]
-    [clojurewerkz.neocons.rest.relationships :as nrl]
-    [clojurewerkz.neocons.rest.cypher :as cy]))
-
-(def conn (nr/connect "http://localhost:7475/db/data/"))
-
-(defn get-person [name]
-  (clojurewerkz.neocons.rest.cypher/tquery conn (str "MATCH (p:Person) WHERE p.name='"name"' RETURN p")))
+(ns sidonnaisuudet.core)
---
+++ src/sidonnaisuudet/neo4j.clj
@@ -0,0 +1,41 @@
+(ns sidonnaisuudet.neo4j
+  (:require
+    [clojurewerkz.neocons.rest :as nr]
+    [clojurewerkz.neocons.rest.nodes :as nn]
+    [clojurewerkz.neocons.rest.relationships :as nrl]
+    [clojurewerkz.neocons.rest.cypher :as cy]))
+
+; Connection details
+(def conn (nr/connect "http://localhost:7475/db/data/"))
+
+(defn cypher [query]
+  (clojurewerkz.neocons.rest.cypher/tquery conn query))
+
+(defn get-entry-by-name [name]
+  (cypher (str "MATCH (n) WHERE n.name='"name"' RETURN n")))
+
+(defn search-entries-by-name [regex]
+  (cypher (str "MATCH (search) WHERE search.name=~'"regex"' RETURN search")))
+
+; Very haxxy way of reading node id, takes cypher return value as an input
+(defn read-id-from-cypher-entry [entry]
+  (Integer/parseInt
+    (last (clojure.string/split
+      (get (second (first (first entry))) :self)
+      #"/"))))
+
+(defn get-node-by-id [id]
+  (nn/get conn id))
+
+(defn get-node-connections-out [id]
+  (nrl/outgoing-for conn (get-node-by-id id)))
+
+(defn get-node-connections-in [id]
+  (nrl/incoming-for conn (get-node-by-id id)))
+
+(defn get-connections-out-by-name [name]
+  (get-node-connections-out (read-id-from-cypher-entry (get-entry-by-name name))))
+
+(defn get-connections-in-by-name [name]
+  (get-node-connections-in (read-id-from-cypher-entry (get-entry-by-name name))))
+
