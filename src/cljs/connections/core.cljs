352c6ca55f0b12fc9b018f801eb4e3c5074495c3
Teemu Frisk <teemu.frisk@gmail.com>
2014-06-07 21:12:36 +0300

    Add search field and basic validation

--- src/cljs/connections/core.cljs
+++ src/cljs/connections/core.cljs
@@ -3,3 +3,20 @@
 (enable-console-print!)
 
 (println "Hello world!")
+
+(defn validate-form []
+  (let [text (.getElementById js/document "search-text")]
+    (if (> (count (.-value text)) 0)
+      true
+      (do (js/alert "Empty search")
+        false))))
+
+;attach validate-form to submit event
+(defn init []
+  ;verify that js/document exists and it has getElementById
+  (if (and js/document (.-getElementById js/document))
+    (set! (.-onsubmit (.getElementById js/document "search-form")) validate-form)))
+
+;init page un unobtrusive way
+(set! (.-onload js/window) init)
+
