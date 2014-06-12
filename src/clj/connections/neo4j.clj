1176e0d6dd31ef64e30952a5c66a9600c66f7adf
Teemu Frisk <teemu.frisk@gmail.com>
2014-06-12 12:07:17 +0300

    Clean search results and remove old files

--- src/clj/connections/neo4j.clj
+++ src/clj/connections/neo4j.clj
@@ -11,18 +11,29 @@
 (defn cypher [query]
   (clojurewerkz.neocons.rest.cypher/tquery conn query))
 
-(defn get-entry-by-name [name]
-  (cypher (str "MATCH (n) WHERE n.name='"name"' RETURN n")))
-
-(defn search-entries-by-name [regex]
-  (cypher (str "MATCH (search) WHERE search.name=~'"regex"' RETURN search")))
-
 ; Very haxxy way of reading node id, takes cypher return value as an input
 (defn read-id-from-cypher-entry [entry]
   (Integer/parseInt
     (last (clojure.string/split
-      (get (second (first (first entry))) :self)
+      (get (second (first entry)) :self)
       #"/"))))
+
+(defn read-name-from-cypher-entry [entry]
+  (get-in (second (first entry)) [:data :name]))
+
+(defn clean-search-results [results]
+  (map
+    #(hash-map
+      :id (read-id-from-cypher-entry %)
+      :name (read-name-from-cypher-entry %))
+    results))
+
+(defn search-entries-by-name [regex]
+  (clean-search-results
+    (cypher (str "MATCH (search) WHERE search.name=~'"regex"' RETURN search"))))
+
+(defn get-entry-by-name [name]
+  (cypher (str "MATCH (n) WHERE n.name='"name"' RETURN n")))
 
 (defn get-node-by-id [id]
   (nn/get conn id))
--- src/connections/core.clj
+++
@@ -1,1 +0,0 @@
-(ns connections.core)
