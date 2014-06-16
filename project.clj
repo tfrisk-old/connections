a3645d5cd01f41b014e6540a50a883a1be81886e
Teemu Frisk <teemu.frisk@gmail.com>
2014-06-16 14:37:07 +0300

    Bump library versions

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2227"]
 		 [clojurewerkz/neocons "3.0.0"]
-		 [compojure "1.1.6"]
-		 [hiccup "1.0.4"]]
+		 [compojure "1.1.8"]
+		 [hiccup "1.0.5"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]
 	    [lein-ring "0.8.7"]]
